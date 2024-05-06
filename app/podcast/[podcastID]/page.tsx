611615f79e4f81867fdc1b732875c3640410b891
Juan <someMail>
2024-05-06 12:14:03 +0200

    feat(PodcastEpisode): add episode count

--- app/podcast/[podcastID]/page.tsx
+++ app/podcast/[podcastID]/page.tsx
@@ -1,28 +1,42 @@
 import getPodcastEpisodeList from "@/app/application/getPodcastEpisodeList";
 import getPodcastList from "@/app/application/getPodcastList";
 import PodcastDetail from "@/stories/podcastDetail/PodcastDetail";
-import classes from './page.module.scss';
+import classes from "./page.module.scss";
 import PodcastEpisodeList from "@/app/ui/sections/PodcastEpisodeList";
 
-async function PodcastDetailPage(context: { params: {podcastID: string}}) {
-
+async function PodcastDetailPage(context: { params: { podcastID: string } }) {
   const podcastID = +context.params.podcastID;
 
-  const podcastList = await getPodcastList({limit: 100, genreID: 1310});
-  const episodeList = await getPodcastEpisodeList({podcastID: podcastID})
-  const podcast = podcastList.find(podcast => podcast.id === podcastID);
+  const podcastList = await getPodcastList({ limit: 100, genreID: 1310 });
+  const episodeList = await getPodcastEpisodeList({ podcastID: podcastID });
+  const podcast = podcastList.find((podcast) => podcast.id === podcastID);
 
-  const episodeListPrimitive = episodeList.map(episode => episode.toPrimitive());
+  const episodeListPrimitive = episodeList.map((episode) =>
+    episode.toPrimitive()
+  );
 
   return (
-      <div className={classes["podcast-detail-page"]}>
-        {podcast &&
-          <PodcastDetail title={podcast.title} author={podcast.author} description={podcast.description} imageSrc={podcast.imageUrl}/>
-        }
-        {episodeList && 
-          <PodcastEpisodeList podcastEpisodes={episodeListPrimitive} podcastID={podcastID}/>
-        }
-      </div>
+    <div className={classes["podcast-detail-page"]}>
+      {podcast && (
+        <PodcastDetail
+          title={podcast.title}
+          author={podcast.author}
+          description={podcast.description}
+          imageSrc={podcast.imageUrl}
+        />
+      )}
+      {episodeList && (
+        <div>
+          <div
+            className={classes["podcast-detail-page__episodes-count"]}
+          >{`Episodes: ${episodeList.length}`}</div>
+          <PodcastEpisodeList
+            podcastEpisodes={episodeListPrimitive}
+            podcastID={podcastID}
+          />
+        </div>
+      )}
+    </div>
   );
 }
 
