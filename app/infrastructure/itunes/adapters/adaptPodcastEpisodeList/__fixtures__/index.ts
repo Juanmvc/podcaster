ebadacb491e4fd457ba832cfb47238aa15f74b2e
Juan <someMail>
2024-05-06 13:25:12 +0200

    test(adaptPodcastEpisodeList): add minimum test for podcast episode adapter

---
+++ app/infrastructure/itunes/adapters/adaptPodcastEpisodeList/__fixtures__/index.ts
@@ -0,0 +1,158 @@
+import PodcastEpisode from "@/app/domain/entities/PodcastEpisode";
+
+export const mockAdaptedPodcastEpisodeList: PodcastEpisode[] = [
+  new PodcastEpisode({
+    id: 1000654401683,
+    title: "Episode 408 w/ Eric Adams (Mayor of New York City)",
+    description:
+      "N.O.R.E. & DJ EFN are the Drink Champs in this episode the champs chop it up with the one and only, Mayor Eric Adams!\n" +
+      "\n" +
+      "The mayor of the most iconic city in the world, New York City, Mayor Eric Adams joins us to share his journey! \n" +
+      "\n" +
+      "Mayor Adams, talks the pressure and responsibilities of being Mayor of New York City. \n" +
+      "\n" +
+      "Mayor Adams, also shares stories of building affordable housing, life in the city through the pandemic, and touches on topics related to Migrants, and much much more!\n" +
+      "\n" +
+      "NY General, Mysonne joins us a special co-host! \n" +
+      "\n" +
+      "Lots of great stories that you don’t want to miss!\n" +
+      "\n" +
+      "Make some noise for Mayor Eric Adams and Mysonne!!! 💐💐💐🏆🏆🏆 🎉🎉🎉\n" +
+      "\n" +
+      " \n" +
+      "\n" +
+      "Sign up for Underdog Fantasy HERE with promo code DRINKCHAMPS and get a $100 first deposit match: https://play.underdogfantasy.com/p-drink-champs\n" +
+      "\n" +
+      "*Subscribe to Patreon NOW for exclusive content, discount codes, M&G’s + more:  🏆*\n" +
+      "\n" +
+      "https://www.patreon.com/drinkchamps\n" +
+      "\n" +
+      "*Listen and subscribe at https://www.drinkchamps.com\n" +
+      "\n" +
+      "Follow Drink Champs:\n" +
+      "\n" +
+      "https://www.instagram.com/drinkchamps\n" +
+      "\n" +
+      "https://www.twitter.com/drinkchamps\n" +
+      "\n" +
+      "https://www.facebook.com/drinkchamps\n" +
+      "\n" +
+      "https://www.youtube.com/drinkchamps\n" +
+      "\n" +
+      "DJ EFN\n" +
+      "\n" +
+      "https://www.crazyhood.com\n" +
+      "\n" +
+      "https://www.instagram.com/whoscrazy\n" +
+      "\n" +
+      "https://www.twitter.com/djefn\n" +
+      "\n" +
+      "https://www.facebook.com/crazyhoodproductions\n" +
+      "\n" +
+      "N.O.R.E.\n" +
+      "\n" +
+      "https://www.instagram.com/therealnoreaga\n" +
+      "\n" +
+      "https://www.twitter.com/noreaga\n" +
+      "See omnystudio.com/listener for privacy information.",
+    releaseDate: new Date("2024-05-03T07:30:00Z"),
+    duration: 7795000,
+    url: "https://chrt.fm/track/BE7515/traffic.omny.fm/d/clips/e73c998e-6e60-432f-8610-ae210140c5b1/9ff2dac3-12fd-4561-b512-ae33005f64f5/36d0dcd3-35aa-449b-ab75-b1640124f955/audio.mp3?utm_source=Podcast&in_playlist=df6d181a-09ea-4bf2-adc6-ae33005f650d",
+  }),
+  new PodcastEpisode({
+    id: 1000653661208,
+    title: "Episode 407 w/ will.i.am",
+    description:
+      "N.O.R.E. & DJ EFN are the Drink Champs. In this episode, the Champs chop it up with the legend himself, will.i.am!will.i.am joins us and shares his journey! Will shares stories of working with Prince, Michael Jackson, Black Eyed Peas and many more!will.i.am talks tech with his latest innovations, FYI.AI, and his partnership with Mercedes as they introduce “MBUX SOUND DRIVE”.Lots of great stories that you don’t want to miss!Make some noise for will.i.am!!! 💐💐💐🏆🏆🏆 🎉🎉🎉\n" +
+      "\n" +
+      "Sign up for Underdog Fantasy HERE with promo code DRINKCHAMPS and get a $100 first deposit match: https://play.underdogfantasy.com/p-drink-champs\n" +
+      "\n" +
+      "*Subscribe to Patreon NOW for exclusive content, discount codes, M&G’s + more:  🏆*\n" +
+      "\n" +
+      "https://www.patreon.com/drinkchamps\n" +
+      "\n" +
+      "*Listen and subscribe at https://www.drinkchamps.com\n" +
+      "\n" +
+      "Follow Drink Champs:\n" +
+      "\n" +
+      "https://www.instagram.com/drinkchamps\n" +
+      "\n" +
+      "https://www.twitter.com/drinkchamps\n" +
+      "\n" +
+      "https://www.facebook.com/drinkchamps\n" +
+      "\n" +
+      "https://www.youtube.com/drinkchamps\n" +
+      "\n" +
+      "DJ EFN\n" +
+      "\n" +
+      "https://www.crazyhood.com\n" +
+      "\n" +
+      "https://www.instagram.com/whoscrazy\n" +
+      "\n" +
+      "https://www.twitter.com/djefn\n" +
+      "\n" +
+      "https://www.facebook.com/crazyhoodproductions\n" +
+      "\n" +
+      "N.O.R.E.\n" +
+      "\n" +
+      "https://www.instagram.com/therealnoreaga\n" +
+      "\n" +
+      "https://www.twitter.com/noreaga\n" +
+      "See omnystudio.com/listener for privacy information.",
+    releaseDate: new Date("2024-04-26T07:30:00Z"),
+    duration: 8432000,
+    url: "https://chrt.fm/track/BE7515/traffic.omny.fm/d/clips/e73c998e-6e60-432f-8610-ae210140c5b1/9ff2dac3-12fd-4561-b512-ae33005f64f5/4a07a3a0-4806-49cc-9ff6-b15c0179dc79/audio.mp3?utm_source=Podcast&in_playlist=df6d181a-09ea-4bf2-adc6-ae33005f650d",
+  }),
+  new PodcastEpisode({
+    id: 1000652947760,
+    title: "Episode 406 w/ FUBU",
+    description:
+      "N.O.R.E. & DJ EFN are the Drink Champs in this episode the champs chop it up with the iconic team from FUBU!\n" +
+      "\n" +
+      "FUBU changed the fashion game and influenced millions when the brand impacted pop culture.\n" +
+      "\n" +
+      "For Us By Us was created by Daymond Jon, Carlton E. Brown, Dr. Keith C Perrin Jr., and J. Alexander Martin. The team joins us to share their journey, the evolution of the brand and much much more!\n" +
+      "\n" +
+      "Lots of great stories that you don’t want to miss!\n" +
+      "\n" +
+      "Make some noise for FUBU!!! 💐💐💐🏆🏆🏆 🎉🎉🎉\n" +
+      "\n" +
+      "Sign up for Underdog Fantasy HERE with promo code DRINKCHAMPS and get a $100 first deposit match: https://play.underdogfantasy.com/p-drink-champs\n" +
+      "\n" +
+      "*Subscribe to Patreon NOW for exclusive content, discount codes, M&G’s + more:  🏆*\n" +
+      "\n" +
+      "https://www.patreon.com/drinkchamps\n" +
+      "\n" +
+      "*Listen and subscribe at https://www.drinkchamps.com\n" +
+      "\n" +
+      "Follow Drink Champs:\n" +
+      "\n" +
+      "https://www.instagram.com/drinkchamps\n" +
+      "\n" +
+      "https://www.twitter.com/drinkchamps\n" +
+      "\n" +
+      "https://www.facebook.com/drinkchamps\n" +
+      "\n" +
+      "https://www.youtube.com/drinkchamps\n" +
+      "\n" +
+      "DJ EFN\n" +
+      "\n" +
+      "https://www.crazyhood.com\n" +
+      "\n" +
+      "https://www.instagram.com/whoscrazy\n" +
+      "\n" +
+      "https://www.twitter.com/djefn\n" +
+      "\n" +
+      "https://www.facebook.com/crazyhoodproductions\n" +
+      "\n" +
+      "N.O.R.E.\n" +
+      "\n" +
+      "https://www.instagram.com/therealnoreaga\n" +
+      "\n" +
+      "https://www.twitter.com/noreaga\n" +
+      "See omnystudio.com/listener for privacy information.",
+    releaseDate: new Date("2024-04-19T07:30:00Z"),
+    duration: 11636000,
+    url: "https://chrt.fm/track/BE7515/traffic.omny.fm/d/clips/e73c998e-6e60-432f-8610-ae210140c5b1/9ff2dac3-12fd-4561-b512-ae33005f64f5/81c76c5f-bc03-47de-a5f9-b15601233b16/audio.mp3?utm_source=Podcast&in_playlist=df6d181a-09ea-4bf2-adc6-ae33005f650d",
+  }),
+];
---
+++ app/infrastructure/itunes/adapters/adaptPodcastEpisodeList/index.test.ts
@@ -0,0 +1,11 @@
+import { mockPodcastEpisodeList } from "../../../../application/getPodcastEpisodeList/__fixtures__/index";
+import adaptPodcastEpisodeList from ".";
+import { mockAdaptedPodcastEpisodeList } from "./__fixtures__";
+
+describe("adaptPodcastEpisodeList adapter", () => {
+  it("should return the Podcasts episodes entities with the correct format", () => {
+    const adapterResult = adaptPodcastEpisodeList(mockPodcastEpisodeList);
+
+    expect(adapterResult).toEqual(mockAdaptedPodcastEpisodeList);
+  });
+});
